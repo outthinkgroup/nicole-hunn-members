d4a99ffd8a1ea953edb5345d19331c6987bd913c
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-23 17:35:24 -0400

    search bar works now

--- components/top-bar/top-bar.php
+++ components/top-bar/top-bar.php
@@ -5,10 +5,13 @@ function util_navigation_and_search(){
   <span class="use-tailwind">
   <div class="top-bar shadow-sm">
     <div class="search-wrapper">
-      <form class="search-bar shadow-sm">
+      <form class="search-bar shadow-sm" action="/" method="GET">
       <input 
         placeholder="Search"
-        type="text"
+        type="search"
+        name="s"
+        value="<?php echo get_search_query() ?>"
+        id="s"
       />
       <button type="submit" >
         <span >
