fa72db7f2611b7c98a01df6998fa2cb25c4f916d
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-30 11:45:32 -0400

    commit

--- assets/index.js
+++ assets/index.js
@@ -310,7 +310,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55972" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59922" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
