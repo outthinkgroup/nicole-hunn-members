e8357fb8e45d6f721a094541e257b56bd858d126
Joseph Hinson <j@outthink.cc>
2020-08-13 14:27:52 -0400

    updated product card for downloads (so we can use post type)

--- components/product-card/products/downloads/downloads.php
+++ components/product-card/products/downloads/downloads.php
@@ -4,9 +4,3 @@ function nhm_user_downloads($user_id){
   $downloads = get_posts('post_type=member_content&posts_per_page=3');
 	return $downloads;
 }
-
-add_filter("product_card_post_type", function($postType, $product){
-
-  $postType = 'member_content';
-  return $postType;
-},10,2);
