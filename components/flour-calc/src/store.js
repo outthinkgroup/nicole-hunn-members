fcf5c409bcf8aa5d0f1358681ef3dacdd8431f5e
Joshua Kennedy <josh@outthinkgroup.com>
2020-08-26 12:39:29 -0400

    changed default recipe to cup4cup

--- components/flour-calc/src/store.js
+++ components/flour-calc/src/store.js
@@ -7,7 +7,7 @@ export const allRecipeNames = _allRecipeNames.map((slug) => {
   return { slug, label: recipes[slug].name };
 });
 
-export const activeRecipeSlug = writable(allRecipeNames[2].slug);
+export const activeRecipeSlug = writable(allRecipeNames[0].slug);
 export const totalCups = writable(1);
 
 export function gramCalc(recipeSlug, totalCups) {
