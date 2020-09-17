15956fbc5780d60f6448713aa7ff148e61e92274
Joshua Kennedy <josh@outthinkgroup.com>
2020-09-17 15:37:45 -0400

    added parameter to insert toggle dynamically with js

--- components/screen-caffeine-toggle/screen-caffeine-toggle.php
+++ components/screen-caffeine-toggle/screen-caffeine-toggle.php
@@ -1,19 +1,51 @@
 <?php
 
 
-function screen_caffeine_toggle(  ) {
-  ob_start(); ?>
-  <div class="wake-lock-toggle">
-    <label class="toggle__label">
-      <input type="checkbox" name="" id="" class="toggle__checkbox" />
-      <div class="toggle__wrapper">
-        <div class="toggle__bg"></div>
-        <div class="toggle__switch"></div>
-      </div>
-    </label>
-  </div>
-  <?php
-  $html = ob_get_clean();
+function screen_caffeine_toggle( $atts ) {
+   $param = shortcode_atts( array(
+    'insert_into' => null,
+    'position'  => 'top-right',
+	), $atts );
+
+  if(!$param['insert_into']){
+    ob_start(); ?>
+    <div class="wake-lock-toggle">
+      <label class="toggle__label">
+        <input type="checkbox" name="" id="" class="toggle__checkbox" />
+        <div class="toggle__wrapper">
+          <div class="toggle__bg"></div>
+          <div class="toggle__switch"></div>
+        </div>
+      </label>
+    </div>
+    <?php
+    $html = ob_get_clean();
+  } else {
+    $parent_selector = $param['insert_into'];
+    ob_start(); ?>
+    <script>
+      const el = document.createElement('div');
+      el.classList.add('wake-lock-toggle')
+      el.classList.add('top-right')
+      
+      el.innerHTML = `
+          <label class="toggle__label">
+            <span style="color:inherit;">Keep Screen From Going to Sleep</span>
+            <input type="checkbox" name="" id="" class="toggle__checkbox" />
+            <div class="toggle__wrapper">
+              <div class="toggle__bg"></div>
+              <div class="toggle__switch"></div>
+            </div>
+          </label>
+      `
+      const parentEl = document.querySelector('<? echo $parent_selector; ?>')
+      if(parentEl){
+        parentEl.appendChild(el)
+      }
+    </script>
+    <?php 
+    $html = ob_get_clean();
+  }
   return $html;
 }
 add_shortcode( 'screen_caffeine', 'screen_caffeine_toggle' );
