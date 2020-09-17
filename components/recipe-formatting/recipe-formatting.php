1618c5641bb8ac84a917d6db87d76d1a39f111a9
Joshua Kennedy <josh@outthinkgroup.com>
2020-09-17 16:11:41 -0400

    added recipe button beside the title

--- components/recipe-formatting/recipe-formatting.php
+++ components/recipe-formatting/recipe-formatting.php
@@ -143,3 +143,11 @@ function gov_get_video_id_from_url($url) {
 		}	
 	}
 }
+
+//Recipe Title filter to add add to collections button beside
+add_filter('the_title', function($title, $id){
+  if(is_single() && ( get_post_type($id) === 'recipe' )){
+    $title = '<span class="title-add-to-collections">' . do_shortcode('[add_recipe_button  icon_button=true]') . '</span> ' . $title;
+  }
+  return $title;
+},10, 2);
