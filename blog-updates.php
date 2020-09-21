c0a09fec7d39a8ea235be1390bfce4f61d82ceb5
Joshua Kennedy <josh@outthinkgroup.com>
2020-09-21 16:01:53 -0400

    member updates archive

--- blog-updates.php
+++ blog-updates.php
@@ -7,15 +7,15 @@ $user = wp_get_current_user();
 $user_id = $user->ID;
 ?>
 
-	<div class="custom-wrapper dashboard">
+	<div class="custom-wrapper page-mx-width">
 		<header>
 			<h2><?php echo the_title(); ?></h2>
 		</header>
 		<aside class="dash-sidebar">
 
 		</aside>
-		<main class="dashboard">
-			<section class="my-downloads">
+		<main class="">
+
 				<ul class="grid">
 				<?php
 						$allposts = get_posts();
@@ -24,7 +24,7 @@ $user_id = $user->ID;
 						}
 					?>
 				</ul>
-			</section>
+
 		</main>
 	</div>
 
