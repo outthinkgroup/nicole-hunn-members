f4ca0517d7527aa9bdb48a9c1e9020b123bf2bce
Joshua Kennedy <josh@outthinkgroup.com>
2020-11-13 12:46:13 -0500

    added a live page template

--- assets/flour-calc/flour-calc.js
+++ assets/flour-calc/flour-calc.js
@@ -4824,7 +4824,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51571" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52664" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
