16e85bc5f542ee4fe039a3b5740f56ea0aebdae1
Joshua Kennedy <josh@outthinkgroup.com>
2020-11-20 14:27:22 -0500

    something bad

--- inc/index.php
+++ inc/index.php
@@ -4,3 +4,4 @@
 include_once NHM_DIR . '/inc/member-bootstrap.php';
 include_once NHM_DIR . '/inc/navigation_setup.php';
 include_once NHM_DIR . '/inc/my_account.php';
+include_once NHM_DIR . '/inc/custom-post-types.php';
