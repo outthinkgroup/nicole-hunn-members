532401332e843edeafd4d87b85ac719eb8cd1823
Joshua Kennedy <josh@outthinkgroup.com>
2020-11-20 14:51:45 -0500

    added custom post type live, and added template

---
+++ inc/custom-post-types.php
@@ -0,0 +1,19 @@
+<?php
+
+//? THE LIVE POST TYPE
+function create_posttype_live() {
+    register_post_type( 'live',
+        array(
+            'labels' => array(
+                'name' => __( 'GFOAS Lives' ),
+                'singular_name' => __( 'GFOAS Lives' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'gfoas-lives'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+add_action( 'init', 'create_posttype_live');
