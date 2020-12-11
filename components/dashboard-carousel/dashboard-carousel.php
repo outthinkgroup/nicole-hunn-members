6a1128437ec3328b58fdf7d2a39f397acdaa0819
Joshua Kennedy <josh@outthinkgroup.com>
2020-12-11 13:38:45 -0500

    conditional link

--- components/dashboard-carousel/dashboard-carousel.php
+++ components/dashboard-carousel/dashboard-carousel.php
@@ -18,7 +18,13 @@ function dashboard_carousel($post_id){
         <div class="content">
           <h4 class="title"><?php echo $slide['title']?></h4>
           <p class="description"><?php echo $slide['description'] ?></p>
-          <a href="<?php echo get_permalink($slide['supporting_post']->ID); ?>" class="btn">
+          <?php if(array_key_exists('supporting_post', $slide) && $slide['supporting_post']){
+              $link = get_permalink($slide['supporting_post']->ID);
+            }elseif(array_key_exists('custom_link', $slide) && $slide['custom_link']) {
+              $link = $slide['custom_link'];
+            }
+            ?>
+          <a href="<?php echo $link; ?>" class="btn">
             
             <?php if(array_key_exists('button_text', $slide) && $slide['button_text']){
               echo $slide['button_text'];
