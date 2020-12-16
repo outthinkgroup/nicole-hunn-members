5856d304f9945cff09d8757619376852eb5209b1
Joshua Kennedy <josh@outthinkgroup.com>
2020-12-16 12:05:59 -0500

    added the notification system to the bootstrap function

--- functions.php
+++ functions.php
@@ -39,44 +39,8 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
  include_once NHM_DIR . '/customfields/customfields.php';
  include_once NHM_DIR . '/inc/index.php';
 
-//TODO: Please move to own file,
-//*CODE That allows us to know if we user has seen  new content
-add_action('save_post', function($post_ID, $post, $update ){
-	if(!get_field('should_notify_users', $post_ID)) return;
-	$users = get_users();
-	foreach($users as $user){
-		$user_id = $user->ID;
-		update_user_meta($user_id, 'should_notify_user', 'true');
-	}
-},10, 3);
 
-function does_user_need_notification($user_id){
-	if(get_user_meta($user_id, 'should_notify_user', true)){
-		$should_notify_user = get_user_meta($user_id, 'should_notify_user', true);
-		return $should_notify_user;
-	}else{
-		return false;
-	}
-}
-
-add_action( 'wp_ajax_nhm_clear_notification', function(){
- 
-  $user_cleared_notification = $_POST['notified'];
-  if($user_cleared_notification){
-    user_was_notified(get_current_user_id());
-    echo 'success';
-  }else{
-    echo 'user did not clear notification';
-  }
-  die();
-} );
-
-function user_was_notified($user_id){
-	return update_user_meta($user_id, 'should_notify_user', 'false');
-}
-
-
-//archive pages
+//archive pages helper
 function get_taxonomy_term_title(){
   return get_queried_object()->name;
 }
