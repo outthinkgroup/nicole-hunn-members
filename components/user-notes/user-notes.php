250db06ca1dd2bc6bc87bc7932cc28d818f7f407
Joshua Kennedy <josh@outthinkgroup.com>
2021-05-04 13:46:45 -0400

    comment form -> note form

--- components/user-notes/user-notes.php
+++ components/user-notes/user-notes.php
@@ -3,13 +3,29 @@
 function gf_get_user_notes(){
   global $post;
   $user = wp_get_current_user();
-  ?>˝
+  ?>
   USER NOTES
   <?php
 }
  // gets the comment form
 function gf_get_note_form(){
-  comment_form($post->ID);
+  ob_start(); ?>
+  <p class="comment-form-comment">
+    <label for="comment">Add A Note</label> 
+    <textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required">
+    </textarea>
+  </p>
+  <?php 
+  $comment_html = ob_get_clean();
+  comment_form([
+    'title_reply_before'=>"",
+    'title_reply'=>"",
+    'title_reply_after'=>"",
+    "logged_in_as"=>"",
+    "comment_field"=>$comment_html,
+    "submit_button"=>'<input name="%1$s" type="submit" id="%2$s" class="%3$s" value="Add Note" />'
+  ]);
+
 }
 
 // Turns on comments for all recipes
