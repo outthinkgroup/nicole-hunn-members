90ff6581eb945b3d16daacdab4e16310daa8b4d9
Joshua Kennedy <josh@outthinkgroup.com>
2021-05-04 13:21:21 -0400

    turned on comments for recipes

--- components/user-notes/user-notes.php
+++ components/user-notes/user-notes.php
@@ -1,13 +1,23 @@
 <?php
  //gets only the users comments
 function gf_get_user_notes(){
-  ?>
+  global $post;
+  $user = wp_get_current_user();
+  ?>˝
   USER NOTES
   <?php
 }
  // gets the comment form
 function gf_get_note_form(){
-  ?>
-  USER NOTES FORM
-  <?php
+  comment_form($post->ID);
+}
+
+// Turns on comments for all recipes
+add_filter( 'comments_open', 'turn_on_comments_for_recipes', 10, 2 );
+function turn_on_comments_for_recipes($open, $post_id){
+  global $post;
+  if(get_post_type($post_id) == 'recipe'){  
+    $open = true;
+  }
+  return $open;
 }
